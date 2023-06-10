d47d55bc74621fdaf5e19029672abade20c58f05
thuvarakan94 <thuvarakan28@gmail.com>
2023-06-10 23:31:35 +0530

    average

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import axios from 'axios'
-import Body from './Reviews'
-import NavbarComponent from './Navbar'
-import {  Reviews } from './types'
-import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem, UncontrolledDropdown, Dropdown } from 'reactstrap';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Body from "./Reviews";
+import NavbarComponent from "./Navbar";
+import { Reviews } from "./types";
+import {
+  ButtonDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  Dropdown,
+} from "reactstrap";
 function App() {
   const [result, setResult] = useState<Reviews[]>([]);
 
@@ -13,51 +20,52 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       const { data } = await axios("http://127.0.0.1:3000/review");
-     
-        setResult(data.review);
-        console.log(data.review);
+
+      setResult(data.review);
+      console.log(data.review);
     };
     fetchData();
   }, []);
 
+  // average rating
+  let Total = 0;
+  let length = result.length;
+  result.forEach(({rating})=> Total += rating);
   return (
     <>
-      <NavbarComponent/>
-    
-<div
-  style={{
-   
-    height: 100,
-    overflow: 'hidden',
-    padding: '8px',
-    width: 300
-  }}
->
+      <NavbarComponent />
 
-  <Dropdown toggle={function noRefCheck(){}}>
-    <DropdownToggle caret>
-      View Selected data
-    </DropdownToggle>
-    <DropdownMenu container="body">
-      <DropdownItem onClick={function noRefCheck(){}}>
-        Action 1
-      </DropdownItem>
-      <DropdownItem onClick={function noRefCheck(){}}>
-        Action 2
-      </DropdownItem>
-      <DropdownItem onClick={function noRefCheck(){}}>
-        Action 3
-      </DropdownItem>
-    </DropdownMenu>
-  </Dropdown>
-</div>
+      <div
+        style={{
+          height: 100,
+          overflow: "hidden",
+          padding: "8px",
+          width: 300,
+        }}
+      >
+        <Dropdown toggle={function noRefCheck() {}}>
+          <DropdownToggle caret>Average Rating {Total/length}</DropdownToggle>
+          <DropdownMenu container="body">
+            <DropdownItem onClick={function noRefCheck() {}}>
+              Low to high
+            </DropdownItem>
+            <DropdownItem onClick={function noRefCheck() {}}>
+              high to low
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </div>
       {result.map((value) => {
         return (
-      <Body stars={value.rating} comment={value.comment} timestamp={value.Date.toString()}/>
-      );
-    })}
+          <Body
+            stars={value.rating}
+            comment={value.comment}
+            timestamp={value.Date.toString()}
+          />
+        );
+      })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
